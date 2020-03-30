521cb49fcdf3c37057f8dbbe94a2f7ddff93dc04
Editon <diicarlos@hotmail.com>
2020-03-30 02:36:27 -0300

    1

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -46,7 +46,7 @@ export default function Register() {
                     <p>Faça seu cadastro na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
                     
                     <Link className="back-link" to="/">
-                    <FiArrowLeft size={16} color="#e02041" />Não tenho cadastro
+                    <FiArrowLeft size={16} color="#e02041" />Voltar para home
                     </Link>
                 </section>
                 <form onSubmit={handleRegister}>
